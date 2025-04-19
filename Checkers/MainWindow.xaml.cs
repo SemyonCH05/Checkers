041428c6c1906f8cdd664690f206f8db230607e5
Minim0zg <baldejlol@gmail.com>
2025-04-19 22:39:33 +0300

    скопировал доску у семена

--- Checkers/MainWindow.xaml.cs
+++ Checkers/MainWindow.xaml.cs
@@ -17,36 +17,84 @@ namespace Checkers
     /// </summary>
     public partial class MainWindow : Window
     {
-        BoardViewModel _boardViewModel;
+        private Dictionary<(int row, int col), Canvas> canvasDict = new Dictionary<(int row, int col), Canvas>();
+        private double cellSize = 58;
         public MainWindow()
         {
             InitializeComponent();
-            _boardViewModel = new BoardViewModel();
-            DataContext = _boardViewModel;
-        }
-        private void StartGame_Click(object sender, RoutedEventArgs e)
-        {
-            MenuScreen.Visibility = Visibility.Collapsed;
-            GameScreen.Visibility = Visibility.Visible;
-        }
-        private void Size(object sender, SizeChangedEventArgs e)
-        {
-            var grid = (Grid)sender;
-            double cellSize = Math.Min(grid.ActualWidth / 8, grid.ActualHeight / 8); 
-            _boardViewModel.UpdateCellSize(cellSize);
-        }
-    }
 
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((i + j) % 2 != 0)
+                    {
+                        var canvas = new Canvas
+                        {
+                            Background = new SolidColorBrush(Color.FromRgb(119, 149, 86)),
+                            Name = $"CheckerCell_{i}_{j}",
+                            HorizontalAlignment = HorizontalAlignment.Stretch,
+                            VerticalAlignment = VerticalAlignment.Stretch,
+                            Width = cellSize,
+                            Height = cellSize
+                        };
+                        canvasDict[(i, j)] = canvas;
+                        Grid.SetRow(canvas, i);
+                        Grid.SetColumn(canvas, j);
+                        grid.Children.Add(canvas);
 
-    // чтобы круг был чуть меньше ячейки
-    public class InnerCheckerSizeConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            => (value is double d) ? d * 0.8 : 0;
+                        int hope = 12;
+                    }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+                }
+
+            }
+        }
+
+        private void ClickMouse(object sender, MouseButtonEventArgs e) // определение положения клетки
         {
-            throw new NotImplementedException();
+            var pos = e.GetPosition((Grid)sender);
+            int row = 0;
+            int col = 0;
+            double accumulatedRow = 0;
+            double accumulatedCol = 0;
+            foreach (var rowDefinition in grid.RowDefinitions)
+            {
+                accumulatedRow += rowDefinition.ActualHeight;
+                if (accumulatedRow >= pos.Y)
+                    break;
+                row++;
+            }
+            foreach (var colDefinition in grid.ColumnDefinitions)
+            {
+                accumulatedCol += colDefinition.ActualWidth;
+                if (accumulatedCol >= pos.X)
+                    break;
+                col++;
+            }
+
+        }
+
+        private void GridSizeChanged(object sender, SizeChangedEventArgs e) // изменение размера всего при изменении размера окна
+        {
+            var gridd = (Grid)sender;
+            double cellSize = Math.Min(gridd.ActualWidth / gridd.ColumnDefinitions.Count, gridd.ActualHeight / gridd.RowDefinitions.Count);
+
+            foreach (var row in grid.RowDefinitions)
+            {
+                row.Height = new GridLength(cellSize);
+            }
+
+            foreach (var column in grid.ColumnDefinitions)
+            {
+                column.Width = new GridLength(cellSize);
+            }
+
+            foreach (var canv in canvasDict.Values)
+            {
+                canv.Width = cellSize;
+                canv.Height = cellSize;
+            }
         }
     }
 }
