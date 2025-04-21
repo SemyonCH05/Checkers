ed076ecf6f1c071b7dc8bd81f5eed5072032a4a9
fifteenfour <82659774+fifteenfour@users.noreply.github.com>
2025-04-21 21:36:11 +0300

    spizdil (#5)
    * создал доску, почти создал сами шашки
    
    * скопировал доску у семена
    
    * Поменял абсолютно все, "привел" к mvvm, создал отдельные Model, ModelView, View. Сделана доска, написан обработчик клика на ячейку. Это просто база, ее надо менять.
    
    * добавил основное меню
    
    ---------
    
    Co-authored-by: Семён Чередов <csemen523@gmail.com>
    Co-authored-by: Minim0zg <baldejlol@gmail.com>

--- Checkers/MainWindow.xaml.cs
+++ Checkers/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,12 +17,36 @@ namespace Checkers
     /// </summary>
     public partial class MainWindow : Window
     {
+        BoardViewModel _boardViewModel;
         public MainWindow()
         {
             InitializeComponent();
-            
-            
+            _boardViewModel = new BoardViewModel();
+            DataContext = _boardViewModel;
+        }
+        private void StartGame_Click(object sender, RoutedEventArgs e)
+        {
+            MenuScreen.Visibility = Visibility.Collapsed;
+            GameScreen.Visibility = Visibility.Visible;
+        }
+        private void Size(object sender, SizeChangedEventArgs e)
+        {
+            var grid = (Grid)sender;
+            double cellSize = Math.Min(grid.ActualWidth / 8, grid.ActualHeight / 8); 
+            _boardViewModel.UpdateCellSize(cellSize);
+        }
+    }
 
+
+    // чтобы круг был чуть меньше ячейки
+    public class InnerCheckerSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => (value is double d) ? d * 0.8 : 0;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Checkers/ModelView.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Checkers
+{
+    class CheckerViewModel : INotifyPropertyChanged
+    {
+        public Checker _checker;
+        public int Row { get; private set; }
+        public int Column { get; private set; }
+        private bool _isKing;
+        public bool IsKing
+        {
+            get => _isKing;
+            set
+            {
+                _isKing = value;
+                OnPropertyChanged("IsKing");
+            }
+        }
+
+        private bool Color => _checker.IsWhite; //цвет шашки 0 - черный 1 - белый
+
+        public Brush Fill => Color ? Brushes.White : Brushes.Black;
+
+        public CheckerViewModel(Checker checker)
+        {
+            _checker = checker;
+            Row = checker.FromX;
+            Column = checker.FromY;
+        }
+
+       
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    class CellViewModel : INotifyPropertyChanged
+    {
+        public int Row { get; }
+        public int Col { get; }
+        public Brush Background => new SolidColorBrush(Color.FromRgb(119, 149, 86));
+        private CheckerViewModel? _checker;
+
+
+        public CheckerViewModel Checker
+        {
+            get => _checker;
+            set
+            {
+                if (_checker != value)
+                {
+                    _checker = value;
+                    OnPropertyChanged("Checker");
+                }
+            }
+
+        }
+
+        private bool isHighlighted;
+        public bool IsHighlighted
+        {
+            get => isHighlighted;
+            set
+            {
+                isHighlighted = value;
+                OnPropertyChanged("IsHighlighted");
+            }
+        }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged("IsSelected");
+            }
+        }
+
+        public ICommand CellClick { get; }
+
+
+        private double _cellSize;
+        public double CellSize
+        {
+            get => _cellSize;
+            set
+            {
+                _cellSize = value;
+                OnPropertyChanged("CellSize");
+            }
+        }
+
+        public CellViewModel(int row, int col, ICommand command)
+        {
+            Row = row;
+            Col = col;
+            CellClick = command;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+
+    class BoardViewModel : INotifyPropertyChanged
+    {
+        private Board _board;
+        private double _cellSize;
+
+        public double CellSize
+        {
+            get => _cellSize;
+            set
+            {
+                if (value != _cellSize)
+                {
+                    _cellSize = value;
+                    OnPropertyChanged("CellSize");
+                }
+            }
+        }
+        public ObservableCollection<CellViewModel> Cells { get; set; }
+        public ICommand CellClickCommand { get; }
+        public CellViewModel _selectedCell;
+        public CellViewModel SelectedCell
+        {
+            get => _selectedCell;
+            set
+            {
+                if (_selectedCell != null) 
+                    _selectedCell.IsSelected = false;
+                _selectedCell = value;
+                if (_selectedCell != null) 
+                    _selectedCell.IsSelected = true;
+                OnPropertyChanged();
+            }
+        }
+
+        public BoardViewModel()
+        {
+
+            CellClickCommand = new RelayCommand(param => OnCellClick((CellViewModel)param));
+
+            _board = new Board();
+            CellSize = 60;
+
+            Cells = new ObservableCollection<CellViewModel>();
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((i + j) % 2 != 0)
+                    {
+                        var cell = new CellViewModel(i, j, CellClickCommand)
+                        {
+                            CellSize = this.CellSize
+                        };
+
+                        var checker = _board.Cells[i, j];
+                        if (checker != null)
+                        {
+                            cell.Checker = new CheckerViewModel(checker);
+                        }
+
+                        Cells.Add(cell);
+                    }
+                }
+            }
+
+            _selectedCell = null;
+        }
+
+        public void UpdateCellSize(double newSize)
+        {
+            foreach (var cell in Cells)
+            {
+                cell.CellSize = newSize;
+            }
+        }
+
+        private void OnCellClick(CellViewModel cell)
+        {
+            // какой-то обработчик нажатия на ячейку пока просто показывает координаты клетки
+            MessageBox.Show(cell.Row.ToString() + " " + cell.Col.ToString());
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object?> _execute;
+        private readonly Func<object?, bool>? _canExecute;
+
+        public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+        public void Execute(object? parameter) => _execute(parameter);
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+
+}
